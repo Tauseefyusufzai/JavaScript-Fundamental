4168d7a7798162c2b657e80c99b8074b1db7aee5
Tauseef Akhtar Yusufzai <138979340+Tauseefyusufzai@users.noreply.github.com>
2023-12-26 10:50:19 +0530

    index.js

--- index.js
+++ index.js
@@ -22,5 +22,40 @@ for(i=0;i<4;i++)
     console.log(fruit[i])
 }*/
 
-let csthird=["Supriya","Tauseef","kriti","sakshi"]
-console.log(csthird[1])
+/*let csthird=["Supriya","Tauseef","kriti","sakshi"]
+console.log(csthird[1])*/
+
+/*let frnd=["Supriya","Swayam","Sakshi","Sameera","kriti","Pranshu","Suraj","Shubi","Om","Sandeep"]
+for(let i=0;i<frnd.length;i++)
+{
+    console.log(`My ${i+1} Friend = ${frnd[i]}`)
+}*/
+
+/*console.log("Table of 7")
+for(let i=1;i<=20;i++)
+{
+    console.log(`7 X ${i} = ${7*i}`)
+}*/
+
+/*let week_1=["play","eat","library","restday","Sport","spritual"]
+let week_2=["footbal","gym","coding","rest","nobel reading","Spritual"]
+console.log("Week 1:")
+for(let i=0;i<week_1.length;i++)
+{
+    console.log(`Day ${i+1} = ${week_1[i]}`)
+}
+console.log("Week 2:")
+for(let i=0;i<week_2.length;i++)
+{
+    console.log(`Day ${i+1} = ${week_2[i]}`)
+}*/
+
+/*let week=[["play","eat","library","restday","Sport","spritual"],["footbal","gym","coding","rest","nobel reading","Spritual"]]
+for(let i=0;i<=1;i++)
+{
+    console.log(`Week ${i+1}`)
+    for(let j=0;j<6;j++)
+    {
+        console.log(`Day ${j+1} = ${week[j]}`)
+    }
+}*/
