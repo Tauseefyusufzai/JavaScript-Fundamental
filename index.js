f1e038b812fe2ded3765459da0d0b504ea12953e
Tauseef Akhtar Yusufzai <138979340+Tauseefyusufzai@users.noreply.github.com>
2023-12-26 09:26:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+/*for(let i=1;i<=7;i++)
+{
+    console.log(`Tauseef Day ${i}`)
+}*/
+
+/*let game = ["Football","Cricket","Kabbadi","Ludo","carrom","KhoKho","chess"]
+n=game.length
+let j=1
+for(let i=0;i<n;i++)
+{
+    console.log(`Tauseef = day ${j++} > ${game[i]}`)
+
+}*/
+
+/*cs_student=["Supriya" , "Tauseef" , "siya" , "Siddhant" , "Swayam"]
+console.log(cs_student[1])*/
+
+/*let fruit=["Banana", "mango","papaya","apple"]
+console.log(fruit.length)
+for(i=0;i<4;i++)
+{
+    console.log(fruit[i])
+}*/
+
+let csthird=["Supriya","Tauseef","kriti","sakshi"]
+console.log(csthird[1])
